784097de0e8bae96222746c3901c701cfaa7411f
JΛVI <hello@soyjavi.com>
2022-03-05 12:41:14 +0700

    New events for primitive <Button>

--- src/primitives/Button/Button.jsx
+++ src/primitives/Button/Button.jsx
@@ -4,12 +4,14 @@ import React from 'react';
 import { styles } from '../helpers';
 import style from './Button.module.css';
 
-const Button = ({ children, disabled, tag = 'button', onPress, ...inherit }) =>
+const Button = ({ children, disabled, tag = 'button', onEnter, onLeave, onPress, ...others }) =>
   React.createElement(
     tag,
     {
-      className: styles(style.primitive, inherit.className),
+      className: styles(style.primitive, !disabled && style.active, others.className),
       onClick: !disabled ? onPress : undefined,
+      onMouseEnter: onEnter,
+      onMouseLeave: onLeave,
     },
     children,
   );
@@ -20,6 +22,8 @@ Button.propTypes = {
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   disabled: PropTypes.bool,
   tag: PropTypes.string,
+  onEnter: PropTypes.func,
+  onLeave: PropTypes.func,
   onPress: PropTypes.func,
 };
 
